e9fdb0dd8606a0ad07fb99c9bc61ddc7f79debed
Bairoking <gs8004rtj5jj5j@gmail.com>
2023-02-06 15:02:59 -0800

    Task 7

--- models/state.py
+++ models/state.py
@@ -25,6 +25,12 @@ class State(BaseModel, Base):
     else:
         name = ''
 
+     def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+
         @property
         def cities(self):
             '''returns the list of City instances with state_id
